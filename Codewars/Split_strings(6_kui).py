992b85035e82f528fb105068d10167222eabfd26
Gromila1984 <karkatarwa2@gmail.com>
2022-07-20 21:30:45 +0300

    добавил описание

--- Codewars/Split_strings(6_kui).py
+++ Codewars/Split_strings(6_kui).py
@@ -1,3 +1,5 @@
+# Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
 def solution(s):
     my_list = []
     result = s if len(s) % 2 == 0 else f"{s}_"
